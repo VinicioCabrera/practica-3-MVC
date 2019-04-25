b9f6666a6311bb16ae16f4f0237acb59e263c724
Usuario <Usuario@DESKTOP-DGPNJFG>
2019-04-24 22:50:50 -0500

    creacion de las clases con los metodos

--- src/ec/edu/ups/clases/Delantero.java
+++ src/ec/edu/ups/clases/Delantero.java
@@ -12,7 +12,7 @@ import ec.edu.ups.interfaz.Interfaz;
  * @version 2.0
  * @since 2019
  * @author Vinicio campoverde
- 
+ * @see http://ups.edu.ec
  */
 public final class Delantero extends Jugador implements Interfaz {
  private int promediogoles;
